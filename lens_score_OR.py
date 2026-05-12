8d39c56aa0bbdbf2299f66991bbc994b950b38c5
JustinWoodham <justinwoodham1@gmail.com>
2026-05-12 19:57:08 +0100

    Added Openrouter pipelines, needed csvs and example extractor

--- lens_score_OR.py
+++ lens_score_OR.py
@@ -1,3 +1,6 @@
+import os
+#os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
 import sys
 import numpy as np
 import pandas as pd
@@ -7,7 +10,7 @@ from lens import download_model, LENS
 csv_path = sys.argv[1] if len(sys.argv) > 1 else "lm_output.csv"
 df = pd.read_csv(csv_path)
 
-# Match the column names from your notebook's CSV output
+# match the column names from pipeline CSV output
 sources = df["Original"].tolist()
 predictions = df["LM Output"].tolist()
 #nested list
@@ -19,8 +22,12 @@ lens_model = LENS(lens_path, rescale=True)
 # run using CPU, saves package issue with CUDA
 scores = lens_model.score(sources, predictions, references, devices =[0])
 
-print(np.mean(scores))
-
+# used to gather few-shot quality metrics
+if "--per-row" in sys.argv:
+    print("\n".join(map(str, scores)))
+else:
+    # returns average for metric score
+    print(np.mean(scores))
 
 # if for any reason lens fails, possibly due to a cache wipe, run command, below with a test csv, and then set devices = None or 0 or 1 or remove
-# python lens_score.py csv\lens_test.csv
+# python lens_score.py csv/lens_test.csv
